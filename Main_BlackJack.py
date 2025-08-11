4f10f19ab28d885099c971e437695d039d32ce08
jared witt <jared.witt@hotmail.com>
2025-08-10 20:00:22 -0400

    compacted code with enable/disable button functions

--- Main_BlackJack.py
+++ Main_BlackJack.py
@@ -9,162 +9,155 @@ import pygwidgets
 from Game import *
 from Constants import *
 
-# 2 - Define constants
-STANDARD_BUTTON_WIDTH = 120
-STANDARD_BUTTON_HEIGHT = 60
-DEAL_BUTTON_WIDTH = STANDARD_BUTTON_WIDTH
-HIT_BUTTON_WIDTH = STANDARD_BUTTON_WIDTH
-BOTTOM_BUTTON_Y = WINDOW_HEIGHT - STANDARD_BUTTON_HEIGHT - MARGIN
-TOP_BUTTON_Y = MARGIN
-RIGHT_BUTTON_X = WINDOW_WIDTH - STANDARD_BUTTON_WIDTH - MARGIN
-LEFT_BUTTON_X = MARGIN
-FRAMES_PER_SECOND = 30
+def disableButtons(buttonList):
+    for TextButton in buttonList:
+        TextButton.disable()
 
-# 3 - Initialize the world
-pygame.init()
-clock = pygame.time.Clock()
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+def enableButtons(buttonList):
+    for TextButton in buttonList:
+        TextButton.enable()
 
-# 4 - Load assets: image(s), sounds,  etc.
-# Images
-background = pygwidgets.Image(window, (0, 0),
-                            'images/background.png') # TODO - replace this image with a custom blackjack table background
-# Buttons
-readyButton = pygwidgets.TextButton(window, (MARGIN, BOTTOM_BUTTON_Y),
-                            'Ready', width=STANDARD_BUTTON_WIDTH, height=STANDARD_BUTTON_HEIGHT)
-hitButtonLeft = WINDOW_CENTER_X - HIT_BUTTON_WIDTH - MARGIN/2
-hitButton = pygwidgets.TextButton(window, (hitButtonLeft, BOTTOM_BUTTON_Y),
-                            'Hit', width=STANDARD_BUTTON_WIDTH, height=STANDARD_BUTTON_HEIGHT)
-standButtonLeft = WINDOW_CENTER_X + MARGIN/2
-standButton = pygwidgets.TextButton(window, (standButtonLeft, BOTTOM_BUTTON_Y),
-                            'Stand', width=STANDARD_BUTTON_WIDTH, height=STANDARD_BUTTON_HEIGHT)
-quitButton = pygwidgets.TextButton(window, (RIGHT_BUTTON_X, BOTTOM_BUTTON_Y),
-                            'Quit', width=STANDARD_BUTTON_WIDTH, height=STANDARD_BUTTON_HEIGHT)
-resetButton = pygwidgets.TextButton(window, (LEFT_BUTTON_X, TOP_BUTTON_Y),
-                                    'Clear Cards', width=STANDARD_BUTTON_WIDTH, height=STANDARD_BUTTON_HEIGHT)
-revealButton = pygwidgets.TextButton(window, (RIGHT_BUTTON_X, TOP_BUTTON_Y),
-                                     'Reveal', width=STANDARD_BUTTON_WIDTH, height=STANDARD_BUTTON_HEIGHT)
-revealButtonLeft, revealButtonTop, revealButtonWidth, revealButtonHeight = revealButton.getRect()
-dealButtonLeft = revealButtonLeft - DEAL_BUTTON_WIDTH - MARGIN
-dealButton = pygwidgets.TextButton(window, (dealButtonLeft, TOP_BUTTON_Y),
-                                     'Deal', width=STANDARD_BUTTON_WIDTH, height=STANDARD_BUTTON_HEIGHT)
-checkDealerButton = pygwidgets.TextButton(window, (WINDOW_CENTER_X - STANDARD_BUTTON_WIDTH/2, TOP_BUTTON_Y),
-                                     'Is Anyone Home?', width=STANDARD_BUTTON_WIDTH, height=STANDARD_BUTTON_HEIGHT) # TODO - put this button in a better place
-checkDealerButton.disable()
-checkDealerButton.hide()
-# TODO - Add buttons for betting
+def main():
+    # 2 - Define constants
+    STANDARD_BUTTON_WIDTH = 120
+    STANDARD_BUTTON_HEIGHT = 60
+    DEAL_BUTTON_WIDTH = STANDARD_BUTTON_WIDTH
+    HIT_BUTTON_WIDTH = STANDARD_BUTTON_WIDTH
+    BOTTOM_BUTTON_Y = WINDOW_HEIGHT - STANDARD_BUTTON_HEIGHT - MARGIN
+    TOP_BUTTON_Y = MARGIN
+    RIGHT_BUTTON_X = WINDOW_WIDTH - STANDARD_BUTTON_WIDTH - MARGIN
+    LEFT_BUTTON_X = MARGIN
+    FRAMES_PER_SECOND = 30
 
-# 5 - Initialize variables
-oGame = Game(window, numberOfPlayers=1)
-lastFrameGameState = ''
+    # 3 - Initialize the world
+    pygame.init()
+    clock = pygame.time.Clock()
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
-# 6 - Loop forever
-while True:
+    # 4 - Load assets: image(s), sounds,  etc.
+    # Images
+    background = pygwidgets.Image(window, (0, 0),
+                                'images/background.png') # TODO - replace this image with a custom blackjack table background
+    # Buttons
+    readyButton = pygwidgets.TextButton(window, (MARGIN, BOTTOM_BUTTON_Y),
+                                'Ready', width=STANDARD_BUTTON_WIDTH, height=STANDARD_BUTTON_HEIGHT)
+    hitButtonLeft = WINDOW_CENTER_X - HIT_BUTTON_WIDTH - MARGIN/2
+    hitButton = pygwidgets.TextButton(window, (hitButtonLeft, BOTTOM_BUTTON_Y),
+                                'Hit', width=STANDARD_BUTTON_WIDTH, height=STANDARD_BUTTON_HEIGHT)
+    standButtonLeft = WINDOW_CENTER_X + MARGIN/2
+    standButton = pygwidgets.TextButton(window, (standButtonLeft, BOTTOM_BUTTON_Y),
+                                'Stand', width=STANDARD_BUTTON_WIDTH, height=STANDARD_BUTTON_HEIGHT)
+    quitButton = pygwidgets.TextButton(window, (RIGHT_BUTTON_X, BOTTOM_BUTTON_Y),
+                                'Quit', width=STANDARD_BUTTON_WIDTH, height=STANDARD_BUTTON_HEIGHT)
+    resetButton = pygwidgets.TextButton(window, (LEFT_BUTTON_X, TOP_BUTTON_Y),
+                                        'Clear Cards', width=STANDARD_BUTTON_WIDTH, height=STANDARD_BUTTON_HEIGHT)
+    revealButton = pygwidgets.TextButton(window, (RIGHT_BUTTON_X, TOP_BUTTON_Y),
+                                         'Reveal', width=STANDARD_BUTTON_WIDTH, height=STANDARD_BUTTON_HEIGHT)
+    revealButtonLeft, revealButtonTop, revealButtonWidth, revealButtonHeight = revealButton.getRect()
+    dealButtonLeft = revealButtonLeft - DEAL_BUTTON_WIDTH - MARGIN
+    dealButton = pygwidgets.TextButton(window, (dealButtonLeft, TOP_BUTTON_Y),
+                                         'Deal', width=STANDARD_BUTTON_WIDTH, height=STANDARD_BUTTON_HEIGHT)
+    checkDealerButton = pygwidgets.TextButton(window, (WINDOW_CENTER_X - STANDARD_BUTTON_WIDTH/2, TOP_BUTTON_Y),
+                                         'Is Anyone Home?', width=STANDARD_BUTTON_WIDTH, height=STANDARD_BUTTON_HEIGHT) # TODO - put this button in a better place
+    checkDealerButton.disable()
+    checkDealerButton.hide()
+    # TODO - Add buttons for betting
 
-    # 7 - Check for and handle events
-    for event in pygame.event.get():
-        if ((event.type == QUIT) or
-            ((event.type == KEYDOWN) and (event.key == K_ESCAPE)) or
-            (quitButton.handleEvent(event))):
-            pygame.quit()
-            sys.exit()
+    # 5 - Initialize variables
+    oGame = Game(window, numberOfPlayers=1)
+    lastFrameGameState = ''
 
-        if readyButton.handleEvent(event):
-            print('clicked Ready')
-            standButton.enable()
-            hitButton.enable()
-            readyButton.disable()
-            oGame.startRound()
+    # 6 - Loop forever
+    while True:
 
-        if hitButton.handleEvent(event):
-            print('clicked Hit')
-            oGame.dealOneCard()
+        # 7 - Check for and handle events
+        for event in pygame.event.get():
+            if ((event.type == QUIT) or
+                ((event.type == KEYDOWN) and (event.key == K_ESCAPE)) or
+                (quitButton.handleEvent(event))):
+                pygame.quit()
+                sys.exit()
 
-        if standButton.handleEvent(event):
-            print('clicked Stand')
-            oGame.nextPlayer()
+            if readyButton.handleEvent(event):
+                print('clicked Ready')
+                standButton.enable()
+                hitButton.enable()
+                readyButton.disable()
+                oGame.startRound()
 
-        if resetButton.handleEvent(event):
-            print('clicked Reset')
-            oGame.nextRound()
+            if hitButton.handleEvent(event):
+                print('clicked Hit')
+                oGame.dealOneCard()
 
-        if revealButton.handleEvent(event):
-            print('clicked Reveal')
-            oGame.revealDealer()
+            if standButton.handleEvent(event):
+                print('clicked Stand')
+                oGame.nextPlayer()
 
-        if dealButton.handleEvent(event):
-            print('clicked Deal')
-            oGame.dealOneCard()
+            if resetButton.handleEvent(event):
+                print('clicked Reset')
+                oGame.nextRound()
 
-        if checkDealerButton.handleEvent(event):
-            print('clicked CheckDealer')
-            oGame.checkDealer()
-            checkDealerButton.disable()
-            checkDealerButton.hide()
+            if revealButton.handleEvent(event):
+                print('clicked Reveal')
+                oGame.revealDealer()
 
-    # 8 - Do any "per frame" actions
-    thisFrameGameState = oGame.getGameState()
-    if thisFrameGameState != lastFrameGameState:
-        if thisFrameGameState == Game.BETTING:
-            standButton.disable()
-            hitButton.disable()
-            readyButton.enable()
-            resetButton.disable()
-            revealButton.disable()
-            dealButton.disable()
-        if thisFrameGameState == Game.DEALING:
-            standButton.disable()
-            hitButton.disable()
-            readyButton.disable()
-            resetButton.disable()
-            revealButton.disable()
-            dealButton.enable()
-        if thisFrameGameState == Game.IS_ANYONE_HOME:
-            checkDealerButton.enable()
-            checkDealerButton.show()
-        if thisFrameGameState == Game.PLAYING:
-            standButton.enable()
-            hitButton.enable()
-            readyButton.disable()
-            resetButton.disable()
-            revealButton.disable()
-            dealButton.disable()
-        if thisFrameGameState == Game.REVEALING:
-            standButton.disable()
-            hitButton.disable()
-            readyButton.disable()
-            resetButton.enable()
-            revealButton.enable()
-            dealButton.disable()
-        if thisFrameGameState == Game.ROUND_OVER:
-            standButton.disable()
-            hitButton.disable()
-            readyButton.disable()
-            resetButton.enable()
-            revealButton.disable()
-            dealButton.disable()
-    lastFrameGameState = oGame.getGameState()
+            if dealButton.handleEvent(event):
+                print('clicked Deal')
+                oGame.dealOneCard()
 
-    oGame.checkBlackJack()
+            if checkDealerButton.handleEvent(event):
+                print('clicked CheckDealer')
+                oGame.checkDealer()
+                checkDealerButton.disable()
+                checkDealerButton.hide()
 
-    # 9 - Clear the window before drawing it again
-    window.fill(BLACK) # TODO - replace this with background.draw() when we have a new image for the background
+        # 8 - Do any "per frame" actions
+        thisFrameGameState = oGame.getGameState()
+        if thisFrameGameState != lastFrameGameState:
+            if thisFrameGameState == Game.BETTING:
+                disableButtons([standButton, hitButton, resetButton, revealButton, dealButton])
+                enableButtons([readyButton])
+            if thisFrameGameState == Game.DEALING:
+                disableButtons([standButton, hitButton, readyButton, resetButton, revealButton])
+                enableButtons([dealButton])
+            if thisFrameGameState == Game.IS_ANYONE_HOME:
+                disableButtons([standButton, hitButton, readyButton, resetButton, revealButton, dealButton])
+                enableButtons([checkDealerButton])
+                checkDealerButton.show()
+            if thisFrameGameState == Game.PLAYING:
+                disableButtons([readyButton, resetButton, revealButton, dealButton])
+                enableButtons([standButton, hitButton])
+            if thisFrameGameState == Game.REVEALING:
+                disableButtons([standButton, hitButton, readyButton, resetButton, dealButton])
+                enableButtons([revealButton])
+            if thisFrameGameState == Game.ROUND_OVER:
+                disableButtons([standButton, hitButton, readyButton, revealButton, dealButton])
+                enableButtons([resetButton, hitButton])
+        lastFrameGameState = oGame.getGameState()
 
-    # 10 - Draw the window elements
-    # Tell the game to draw itself
-    oGame.draw()
-    # Draw remaining user interface components
-    readyButton.draw()
-    hitButton.draw()
-    standButton.draw()
-    quitButton.draw()
-    resetButton.draw()
-    revealButton.draw()
-    dealButton.draw()
-    checkDealerButton.draw()
+        oGame.checkBlackJack()
 
-    # 11 - Update the window
-    pygame.display.update()
+        # 9 - Clear the window before drawing it again
+        window.fill(BLACK) # TODO - replace this with background.draw() when we have a new image for the background
 
-    # 12 - Slow things down a bit
-    clock.tick(FRAMES_PER_SECOND)
+        # 10 - Draw the window elements
+        # Tell the game to draw itself
+        oGame.draw()
+        # Draw remaining user interface components
+        readyButton.draw()
+        hitButton.draw()
+        standButton.draw()
+        quitButton.draw()
+        resetButton.draw()
+        revealButton.draw()
+        dealButton.draw()
+        checkDealerButton.draw()
+
+        # 11 - Update the window
+        pygame.display.update()
+
+        # 12 - Slow things down a bit
+        clock.tick(FRAMES_PER_SECOND)
+
+if __name__ == '__main__':
+    main()
